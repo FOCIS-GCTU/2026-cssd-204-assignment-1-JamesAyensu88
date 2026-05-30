1b406546ddce0b7bfbdc3d95bc8f80381ebd765b
JAMES AMANIAMPONG AYENSU <2425401452@live.gctu.edu.gh>
2026-05-30 14:00:45 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,35 +1,7 @@
-#File: initials.py
-#Description: Printing out my initials
-#Assignment Number: 1
-#
-#Name: James Amaniampong Ayensu
-#STUDENT ID: 2425401452
-#Email: 2425401452@live.gctu.edu.gh
-#Grader: Agustus Buckman
-#
-#On my honor, James Amaniampong Ayensu, this programming assignment is my own work
-#and I have not provided this code to any other student.
-
-#this function prints the letter in Uppercases
 def main():
   pass #code goes here
-  print()
-    print ("...JAA")
-    print()
-    print("...JJJJJJJJJJJJ............AAAA................AAAA.........")
-    print("...JJJJJJJJJJJJ............AAAA................AAAA.........")
-    print("........JJJJ..............AA..AA..............AA..AA........")
-    print("........JJJJ..............AA..AA..............AA..AA........")
-    print("........JJJJ.............AAAAAAAA............AAAAAAAA.......")
-    print("........JJJJ.............AAAAAAAA............AAAAAAAA.......")
-    print("...JJ...JJJJ............AA......AA..........AA......AA......")
-    print("...JJ...JJJJ............AA......AA..........AA......AA......")
-    print("...JJJJJJJJ............AA........AA........AA........AA.....")
-    print("...JJJJJJJJ............AA........AA........AA........AA.....")
-    print("")
+  
 
-if __name__ == "__main__":
-    main()
 
 
 
